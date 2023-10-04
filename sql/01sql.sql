8725a3b4a43c4bb4750d3ec158e5c43e006b762b
LeeJungHoon <gns14585@naver.com>
2023-10-04 10:56:26 +0900

    2023-10-04

---
+++ sql/01sql.sql
@@ -0,0 +1,10 @@
+-- 주석
+# 주석
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+-- sql은 대소문자 구분 안함
+SELECT *
+FROM w3schools.CUSTOMERS;
+-- 키워드는 대문자, 나머지는 소문자 (관습, 엄격하지 않음)
---
+++ sql/02select.sql
@@ -0,0 +1,57 @@
+-- SELECT : 데이터 조회(읽기, 검색, 가져오기 등등)
+-- ; 세미콜론을 기준으로 다음 쿼리문 실행
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.employees;
+SELECT * FROM w3schools.products;
+
+SELECT * FROM customers;
+SELECT * FROM customers;
+
+SELECT * FROM categories;
+SELECT * FROM orderdetails;
+SELECT * FROM orders;
+SELECT * FROM products;
+SELECT * FROM shippers;
+SELECT * FROM suppliers;
+
+--
+SELECT CustomerName -- column(열) 명 나열
+FROM customers-- table 명 나열
+;
+
+SELECT CustomerID, CustomerName FROM customers;
+-- 출력되는 테이블 순서는 쿼리문 작성 방식에 어떤걸 먼저 작성할지에 따라 다름
+SELECT CustomerID, CustomerName, City FROM customers;
+SELECT CustomerName, CustomerID, Country FROM customers;
+
+-- 전체 * culumn(열) 표시
+SELECT * FROM customers;
+
+-- 1) 직원(employees)의 lastName, firstName, birthDate 조회
+SELECT LastName, FirstName, BirthDate FROM employees;
+
+-- 2) 상품(products)의 이름(productName), 단위(unit), 가격(price) 조회
+SELECT ProductName, Unit FROM products;
+
+-- 3) 공급자(suppliers)의 이름(supplierName), 주소(address), 국가(country) 조회
+SELECT SupplierName, Address, Country FROM suppliers;
+
+-- 고객테이블의 국가 컬럼 조회
+SELECT Country FROM customers;
+
+-- DISTINCT 중복 제거
+SELECT DISTINCT Country FROM customers; -- 21 ROW
+
+SELECT City, Country FROM customers; -- 91 ROW
+SELECT DISTINCT City, Country FROM customers; -- 69 ROW
+
+-- COUNT : 행(ROW, RECORD)의 수
+SELECT COUNT(Country) FROM customers; -- 91
+SELECT COUNT(DISTINCT Country) FROM customers; -- 21
+
+-- 전체 행의 수
+SELECT COUNT(*) FROM customers;
+SELECT COUNT(*) FROM employees;
+SELECT COUNT(*) FROM orders;
+
+
