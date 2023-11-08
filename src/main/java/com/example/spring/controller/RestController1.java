3d56fc1d7ad52b67ae03eb1b4b9423b88467daa2
LeeJungHoon <gns14585@naver.com>
2023-11-08 12:31:38 +0900

    @RestController는 @Controller와 @ResponseBody의 결합

--- src/main/java/com/example/spring/controller/RestController1.java
+++ src/main/java/com/example/spring/controller/RestController1.java
@@ -9,10 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+//@Controller
+//@ResponseBody // 모든 메소드에 적용됨
+@RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 @RequestMapping("api/main1")
 public class RestController1 {
@@ -21,7 +24,6 @@ public class RestController1 {
 
     // http://localhost:8080/api/main1/sub1
     @GetMapping("sub1")
-    @ResponseBody
     public String method1() {
 
         return "customer name!!";
@@ -32,21 +34,18 @@ public class RestController1 {
     // 5번 고객의 customerName 응답
 
     @GetMapping("sub2")
-    @ResponseBody
     public String method2() {
 
         return dao.selectCustomerId();
     }
 
     @GetMapping("sub3")
-    @ResponseBody
     public MyDto34Customer method3() {
         return dao.getCustomer();
     }
 
 
     @GetMapping("sub4")
-    @ResponseBody
     public ResponseEntity<Object> method4(Integer id) {
         MyDto34Customer customer = dao.getCustomerById(id);
         if (customer == null) {
@@ -56,7 +55,6 @@ public class RestController1 {
     }
 
     @GetMapping("sub5")
-    @ResponseBody
     public ResponseEntity<Object> method5(Integer id) {
         MyDto33Employee employee = dao.getEmployeeById(id);
         if (employee == null) {
@@ -66,13 +64,11 @@ public class RestController1 {
     }
 
     @GetMapping("sub6")
-    @ResponseBody
     public List<Integer> method6() {
         return dao.getCustomerIdList();
     }
 
     @GetMapping("sub7")
-    @ResponseBody
     public List<Integer> method7() {
         return dao.getEmployeeIdList();
     }
